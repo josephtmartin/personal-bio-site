f8247d6104d3aa07e271706e9a857a66c388a462
josephtmartin <goldglovejo13@gmail.com>
2020-07-22 23:50:41 -0500

    added to projects.js and is working

--- project.js
+++ project.js
@@ -1,1 +1,58 @@
-console.log("Lorem Ipsum")
+"use strict";
+
+const projects = [
+  {
+    title: "Cool Project",
+    // screenshot: "http://gotoflashgames.com/files/file/033.jpg",
+    screenshot:
+      "https://pbs.twimg.com/profile_images/1097349761189859330/5652XPXc_400x400.jpg",
+    description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed:
+      "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/josephtmartin", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: "https://github.com/josephtmartin",
+  },
+
+  {
+    title: "Cool Project",
+    // screenshot: "http://gotoflashgames.com/files/file/033.jpg",
+    screenshot:
+      "https://pbs.twimg.com/profile_images/1097349761189859330/5652XPXc_400x400.jpg",
+    description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed:
+      "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: false,
+    url: "https://github.com/josephtmartin", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: "https://github.com/josephtmartin",
+  },
+];
+
+const printToDom = (divId, textToPrint) => {
+  const selectedDiv = document.getElementById(divId);
+  selectedDiv.innerHTML = textToPrint;
+};
+
+const createProjectCards = () => {
+  let domString = "";
+
+  for (let i = 0; i < projects.length; i++) {
+    domString += `<div class="project">`;
+    domString += `<div class="project-title">${projects[i].title}</div>`;
+    domString += `<div><img src=${projects[i].screenshot} alt="screenshot"></div>`;
+    domString += `<div>${projects[i].description}</div>`;
+    domString += `<div>${projects[i].technologiesUsed}</div>`;
+    // domString += `<div>${projects[i].available}</div>`;
+    domString += `<div>Get the project <a href=${projects[i].url}>HERE</a></div>`;
+    domString += `<div>Get my GitHub Profile<a href=${projects[i].githubUrl}>HERE</a></div>`;
+    domString += `</div>`;
+  }
+
+//   if (projects.available === true) {
+//     printToDom("projectsPage", domString);
+//   }
+
+  printToDom("projectsPage", domString);
+};
+
+createProjectCards();
